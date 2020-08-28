b9f970ad5446071ecec12ce4065877b0458f0874
Adam Simpson <adam@heysparkbox.com>
2020-08-28 14:07:27 -0400

    feat: require pygments to be present at build time.

---
+++ ci/linux.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update -qy;
+sudo apt install -qy curl python3 zlib1g-dev sbcl build-essential git;
+git clone --depth 1 -b sbcl-2.0.7 git://git.code.sf.net/p/sbcl/sbcl /tmp/sbcl;
+cd /tmp/sbcl || exit;
+sh make.sh --with-sb-core-compression;
+sudo sh install.sh;
+sudo apt remove -yq sbcl;
